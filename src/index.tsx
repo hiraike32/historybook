f1643a30cdb4616c6d8f08a58c0974164930aea8
平池拓也 <sssb.th.63332@gmail.com>
2018-11-11 11:47:43 +0900

    全てをコンポーネントに移して、reduxを削除

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
-import store from "./store";
-import Root from "./containers/Root";
+import Root from "./components/Root";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Root />
-    </BrowserRouter>
-  </Provider>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Root />
+  </BrowserRouter>,
   document.getElementById("app")
 );
